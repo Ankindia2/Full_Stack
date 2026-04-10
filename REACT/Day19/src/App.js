1eea3655640c51f7e5f10c46f9217548374b3002
Ankit Kumar <samratankit55@gmail.com>
2026-04-10 23:29:20 +0530

    Add file Talwind_CSS

---
+++ REACT/Day19/src/App.js
@@ -0,0 +1,138 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+function App(){
+    
+    return(
+       <div className="flex gap-2 flex-wrap justify-center">
+       <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+        <div className="max-w-sm overflow-hidden bg-amber-600  ">
+        <div >
+            <img className="w-full h-48 object-cover rounded-2xl" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" ></img>
+        </div > 
+        <h1>Biohgraphy</h1>
+        
+       <div className="mt-2 font-bold text-2xl text-gray-700">
+        <p>Ankit samrat is one of the best programmer</p>
+       </div>
+       <div className="mt-3 flex justify-center ">
+        <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-700">Know More</button>
+       </div>
+       
+       </div>
+       </div>
+    )
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
